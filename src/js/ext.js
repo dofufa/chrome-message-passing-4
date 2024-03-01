189eb4a4dce04b0c01985b0b3ba871fab81db4b8
Dofufa Labs <121826629+dofufa@users.noreply.github.com>
2024-03-01 12:06:05 -0800

    Update ext.js

--- src/js/ext.js
+++ src/js/ext.js
@@ -4,12 +4,12 @@
 // repo : https://github.com/dofufa/chrome-message-passing-2
 console.log('ext-content-script');
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    const reply = {name: 'from-content-script', sriracha: true};
-    console.log('message received by content script, from service worker')
-    console.log('sender: %o', sender);
-    console.log('request: %o', request);
-    console.log();
-    return sendResponse(reply);
-  });
+const message = { name: 'from-content script',
+  options: { ramen: ['string', 'string', 'string'] }
+};
+
+chrome.runtime.sendMessage(message, (response) => {
+  console.log('response received by content script, from service worker')
+  console.log('response: %o', response);
+  console.log();
+});
